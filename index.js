0c4003241f78592012f1890a4477497c79369aca
Sunaina_dicy <100867824+Sunainante@users.noreply.github.com>
2022-12-12 15:06:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var count =0;
+
+function myFunction() {
+    // var x = document.getElementById("para");
+    // if (x.innerHTML === "Disappear") {
+    //   x.innerHTML = "";
+    // } else {
+    //   x.innerHTML = "Disappear";
+    // }
+    count++;
+    var x = document.getElementById("para");
+    x.innerHTML = count;
+  }
